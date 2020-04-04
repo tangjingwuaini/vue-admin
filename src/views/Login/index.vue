9c5dfd455eb76f0e9d0d942d7ccd804d403c43b5
tangjingwuaini <981458096@qq.com>
2020-04-05 02:04:25 +0800

    构建项目,配置全局样式,路由,elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        这是登录页
+        <el-row>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+    </div>
+</template>
+<script>
+export default {
+    name:'app'
+}
+</script>
+<style lang="css" scoped>
+#app{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
